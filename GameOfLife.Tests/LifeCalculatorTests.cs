aa0e25edc8f29aa3062e14f916d559b84b29f891
Darren Cauthon <darren@cauthon.com>
2017-09-12 11:43:59 -0500

    First passing test.

---
+++ GameOfLife.Tests/LifeCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMoq.Helpers;
+using GameOfLife;
+using NUnit.Framework;
+using Should;
+
+namespace GameOfLife.Tests
+{
+    public class LifeCalculatorTests
+    {
+        [TestFixture]
+        public class ShouldBeAlive
+        {
+            private LifeCalculator lifeCalculator;
+
+            [SetUp]
+            public void Setup()
+            {
+                lifeCalculator = new LifeCalculator();
+            }
+
+            //Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+            [Test]
+            public void A_live_cell_with_no_live_neighbors_dies()
+            {
+                var cell = new Cell
+                {
+                    IsAlive = true,
+                    Neighbors = new Cell[] {}
+                };
+
+                lifeCalculator.ShouldBeAlive(cell).ShouldBeFalse();
+            }
+            
+        }
+    }
+}
---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Cell
+    {
+        public bool IsAlive { get; set; }
+        public IEnumerable<Cell> Neighbors { get; set; }
+    }
+}
